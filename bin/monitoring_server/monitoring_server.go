6ed9f42cbfbbb7c151432560fbaf5ac676774daf
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-12 00:32:15 +0200

    add monitoring server

---
+++ bin/monitoring_server/monitoring_server.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+
+	"time"
+
+	io_util "github.com/bborbe/io/util"
+	"github.com/bborbe/lock"
+	"github.com/bborbe/log"
+	"github.com/bborbe/mailer"
+	mail_config "github.com/bborbe/mailer/config"
+	monitoring_check "github.com/bborbe/monitoring/check"
+	monitoring_configuration_parser "github.com/bborbe/monitoring/configuration_parser"
+	monitoring_node "github.com/bborbe/monitoring/node"
+	monitoring_notifier "github.com/bborbe/monitoring/notifier"
+	monitoring_runner_hierarchy "github.com/bborbe/monitoring/runner/hierarchy"
+	"github.com/bborbe/webdriver"
+)
+
+var logger = log.DefaultLogger
+
+const (
+	PARAMETER_LOGLEVEL = "loglevel"
+	PARAMETER_CONFIG = "config"
+	PARAMETER_DRIVER = "driver"
+	DEFAULT_LOCK = "~/.monitoring_cron.lock"
+	PARAMETER_DELAY = "delay"
+	DEFAULT_DELAY = time.Hour * 12
+	PARAMETER_SMTP_USER = "smtp-user"
+	PARAMETER_SMTP_PASSWORD = "smtp-password"
+	PARAMETER_SMTP_HOST = "smtp-host"
+	PARAMETER_SMTP_PORT = "smtp-port"
+	PARAMETER_SMTP_SENDER = "sender"
+	PARAMETER_SMTP_RECIPIENT = "recipient"
+	PARAMETER_CONCURRENT = "max"
+	PARAMETER_LOCK = "lock"
+)
+
+type Run func(nodes []monitoring_node.Node) <-chan monitoring_check.CheckResult
+
+type Notify func(results []monitoring_check.CheckResult) error
+
+type ParseConfiguration func(content []byte) ([]monitoring_node.Node, error)
+
+func main() {
+	defer logger.Close()
+	logLevelPtr := flag.String(PARAMETER_LOGLEVEL, log.LogLevelToString(log.ERROR), log.FLAG_USAGE)
+	configPtr := flag.String(PARAMETER_CONFIG, "", "config")
+	driverPtr := flag.String(PARAMETER_DRIVER, "phantomjs", "driver phantomjs|chromedriver")
+	smtpUserPtr := flag.String(PARAMETER_SMTP_USER, "smtp@benjamin-borbe.de", "string")
+	smtpPasswordPtr := flag.String(PARAMETER_SMTP_PASSWORD, "-", "string")
+	smtpHostPtr := flag.String(PARAMETER_SMTP_HOST, "iredmail.mailfolder.org", "string")
+	smtpPortPtr := flag.Int(PARAMETER_SMTP_PORT, 465, "int")
+	senderPtr := flag.String(PARAMETER_SMTP_SENDER, "smtp@benjamin-borbe.de", "string")
+	recipientPtr := flag.String(PARAMETER_SMTP_RECIPIENT, "bborbe@rocketnews.de", "string")
+	maxConcurrencyPtr := flag.Int(PARAMETER_CONCURRENT, runtime.NumCPU(), "max concurrency")
+	lockNamePtr := flag.String(PARAMETER_LOCK, DEFAULT_LOCK, "lock file")
+	delayPtr := flag.Duration(PARAMETER_DELAY, DEFAULT_DELAY, "delay")
+
+	flag.Parse()
+	logger.SetLevelThreshold(log.LogStringToLevel(*logLevelPtr))
+	logger.Debugf("set log level to %s", *logLevelPtr)
+
+	logger.Debugf("max concurrency: %d", *maxConcurrencyPtr)
+
+	var driver webdriver.WebDriver
+	if *driverPtr == "chromedriver" {
+		driver = webdriver.NewChromeDriver("chromedriver")
+	} else {
+		driver = webdriver.NewPhantomJsDriver("phantomjs")
+	}
+	driver.Start()
+	defer driver.Stop()
+
+	mailConfig := mail_config.New()
+	mailConfig.SetSmtpUser(*smtpUserPtr)
+	mailConfig.SetSmtpPassword(*smtpPasswordPtr)
+	mailConfig.SetSmtpHost(*smtpHostPtr)
+	mailConfig.SetSmtpPort(*smtpPortPtr)
+	runner := monitoring_runner_hierarchy.New(*maxConcurrencyPtr)
+	mailer := mailer.New(mailConfig)
+	notifier := monitoring_notifier.New(mailer, *senderPtr, *recipientPtr)
+	configurationParser := monitoring_configuration_parser.New(driver)
+
+	err := do(runner.Run, notifier.Notify, configurationParser.ParseConfiguration, *configPtr, *lockNamePtr, *delayPtr)
+	if err != nil {
+		logger.Fatal(err)
+		logger.Close()
+		os.Exit(1)
+	}
+	logger.Debug("done")
+}
+
+func do(run Run, notify Notify, parseConfiguration ParseConfiguration, configPath string, lockName string, delay time.Duration) error {
+	var err error
+	lockName, err = io_util.NormalizePath(lockName)
+	if err != nil {
+		return err
+	}
+	logger.Debugf("try locking %s", lockName)
+	l := lock.NewLock(lockName)
+	if err = l.Lock(); err != nil {
+		logger.Debugf("lock %s failed: %v", lockName, err)
+		return err
+	}
+	defer l.Unlock()
+
+	if len(configPath) == 0 {
+		return fmt.Errorf("parameter {} missing", PARAMETER_CONFIG)
+	}
+	path, err := io_util.NormalizePath(configPath)
+	if err != nil {
+		return err
+	}
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	nodes, err := parseConfiguration(content)
+	if err != nil {
+		return err
+	}
+
+	for {
+		logger.Debugf("check started")
+		results := make([]monitoring_check.CheckResult, 0)
+		failedChecks := 0
+		var result monitoring_check.CheckResult
+		for result = range run(nodes) {
+			if !result.Success() {
+				failedChecks++
+			}
+			results = append(results, result)
+		}
+		logger.Debugf("all checks executed")
+		if failedChecks > 0 {
+			err = notify(results)
+			if err != nil {
+				return err
+			}
+		}
+		logger.Debugf("check finished")
+
+		logger.Debugf("sleep for %v", delay)
+		time.Sleep(delay)
+	}
+
+	return err
+}
---
+++ bin/monitoring_server/monitoring_server_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	. "github.com/bborbe/assert"
+	monitoring_check "github.com/bborbe/monitoring/check"
+	monitoring_node "github.com/bborbe/monitoring/node"
+)
+
+func TestDoEmpty(t *testing.T) {
+	writer := bytes.NewBufferString("")
+	err := do(writer, func(nodes []monitoring_node.Node) <-chan monitoring_check.CheckResult {
+		return nil
+	}, func(results []monitoring_check.CheckResult) error {
+		return nil
+	}, func(content []byte) ([]monitoring_node.Node, error) {
+		return nil, nil
+	}, "", "/tmp/lock")
+	if err = AssertThat(err, NotNilValue()); err != nil {
+		t.Fatal(err)
+	}
+}
