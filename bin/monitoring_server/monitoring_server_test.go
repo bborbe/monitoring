f502f433f37704f9b8343a5ee1d6550153c470ae
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-23 21:35:17 +0100

    refactoring

--- bin/monitoring_server/monitoring_server_test.go
+++ bin/monitoring_server/monitoring_server_test.go
@@ -9,21 +9,28 @@ import (
 	. "github.com/bborbe/assert"
 	monitoring_check "github.com/bborbe/monitoring/check"
 	monitoring_node "github.com/bborbe/monitoring/node"
+	"context"
 )
 
 func TestDoSendNoMail(t *testing.T) {
 	counter := 0
-	err := do(func(nodes []monitoring_node.Node) <-chan monitoring_check.CheckResult {
-		c := make(chan monitoring_check.CheckResult, 1)
-		c <- monitoring_check.NewCheckResultSuccess("ok", time.Millisecond)
-		close(c)
-		return c
-	}, func(sender string, recipient string, subject string, results []monitoring_check.CheckResult) error {
-		counter++
-		return nil
-	}, func(content string) ([]monitoring_node.Node, error) {
-		return nil, nil
-	}, "config.xml", "/tmp/lock", time.Hour, "",true, "a", "b", "c")
+	s := &server{
+		runner: func(nodes []monitoring_node.Node) <-chan monitoring_check.CheckResult {
+			c := make(chan monitoring_check.CheckResult, 1)
+			c <- monitoring_check.NewCheckResultSuccess("ok", time.Millisecond)
+			close(c)
+			return c
+		},
+		notify: func(sender string, recipient string, subject string, results []monitoring_check.CheckResult) error {
+			counter++
+			return nil
+		},
+		parseNodes: func(content string) ([]monitoring_node.Node, error) {
+			return nil, nil
+		},
+		oneTime: true,
+	}
+	err := s.run(context.Background())
 	if err := AssertThat(err, NilValue()); err != nil {
 		t.Fatal(err)
 	}
@@ -34,17 +41,23 @@ func TestDoSendNoMail(t *testing.T) {
 
 func TestDoSendMail(t *testing.T) {
 	counter := 0
-	err := do(func(nodes []monitoring_node.Node) <-chan monitoring_check.CheckResult {
-		c := make(chan monitoring_check.CheckResult, 1)
-		c <- monitoring_check.NewCheckResultFail("ok", fmt.Errorf("foo"), time.Millisecond)
-		close(c)
-		return c
-	}, func(sender string, recipient string, subject string, results []monitoring_check.CheckResult) error {
-		counter++
-		return nil
-	}, func(content string) ([]monitoring_node.Node, error) {
-		return nil, nil
-	}, "config.xml", "/tmp/lock", time.Hour, "",true, "a", "b", "c")
+	s := &server{
+		runner: func(nodes []monitoring_node.Node) <-chan monitoring_check.CheckResult {
+			c := make(chan monitoring_check.CheckResult, 1)
+			c <- monitoring_check.NewCheckResultFail("ok", fmt.Errorf("foo"), time.Millisecond)
+			close(c)
+			return c
+		},
+		notify: func(sender string, recipient string, subject string, results []monitoring_check.CheckResult) error {
+			counter++
+			return nil
+		},
+		parseNodes: func(content string) ([]monitoring_node.Node, error) {
+			return nil, nil
+		},
+		oneTime: true,
+	}
+	err := s.run(context.Background())
 	if err := AssertThat(err, NilValue()); err != nil {
 		t.Fatal(err)
 	}
